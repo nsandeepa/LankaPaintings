6867999f07f45f596cac14efa3bae3b0f1691f14
Nilupul Sandeepa <nilupul@4axissolutions.com>
2019-11-26 12:56:00 +0530

    Initial Commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "", component: DashboardComponent, children: [
+    {path: "", component: HomeComponent}
+  ]}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
